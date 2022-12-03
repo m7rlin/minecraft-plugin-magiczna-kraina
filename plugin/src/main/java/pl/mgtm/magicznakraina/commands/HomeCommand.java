6dac6bab575ad9e64c7ac9c726ce97b1c298ac1b
Fulerenn <maciej15s12@gmail.com>
2022-12-03 01:25:16 +0100

    Typo fixes and better messages

--- plugin/src/main/java/pl/mgtm/magicznakraina/commands/HomeCommand.java
+++ plugin/src/main/java/pl/mgtm/magicznakraina/commands/HomeCommand.java
@@ -10,42 +10,35 @@ import pl.mgtm.magicznakraina.command.PluginCommand;
 
 @CommandInfo(name = "home", permission = "", requiresPlayer = true)
 public class HomeCommand extends PluginCommand {
-
-    private MagicznaKraina pl = MagicznaKraina.getInstance();
+    private MagicznaKraina plugin = MagicznaKraina.getInstance();
 
     @Override
     public void execute(Player player, String[] args) {
         super.execute(player, args);
 
-        FileConfiguration config = pl.getConfig();
+        FileConfiguration config = plugin.getConfig();
 
+        // Sprawdz czy dom jest ustawiony
+        if (config.contains("users." + player.getUniqueId() + ".home")) {
+            String worldName = config.getString("users." + player.getUniqueId() + ".home.world");
 
-        if (args.length == 0) {
-            // Sprawdz czy dom jest ustawiony
-            if (config.contains("users." + player.getUniqueId() + ".home")) {
-                String worldName = config.getString("users." + player.getUniqueId() + ".home.world");
-
-                if (!player.getWorld().getName().equals(worldName)) {
-                    player.sendMessage(ChatColor.RED + "Nie możesz teleportować się pomiedzy wymiarami. :(");
-                    return;
-                }
-
-                double x = config.getDouble("users." + player.getUniqueId() + ".home.x");
-                double y = config.getDouble("users." + player.getUniqueId() + ".home.y");
-                double z = config.getDouble("users." + player.getUniqueId() + ".home.z");
-                float pitch = (float) config.getDouble("users." + player.getUniqueId() + ".home.pitch");
-                float yaw = (float) config.getDouble("users." + player.getUniqueId() + ".home.yaw");
-
-                Location loc = new Location(player.getWorld(), x, y, z, yaw, pitch);
-
-                player.teleport(loc);
-                player.sendMessage(ChatColor.GREEN + "Przeniesiono do domu.");
-            } else {
-                player.sendMessage("§cNie posiadasz pozycji domu.\n§7Użyj §c/sethome §7aby ustawić pozycję domu.");
+            if (!player.getWorld().getName().equals(worldName)) {
+                player.sendMessage(ChatColor.RED + "Nie możesz teleportować się pomiedzy wymiarami. :(");
+                return;
             }
-        } else {
-            player.sendMessage("§c/home");
-        }
 
+            double x = config.getDouble("users." + player.getUniqueId() + ".home.x");
+            double y = config.getDouble("users." + player.getUniqueId() + ".home.y");
+            double z = config.getDouble("users." + player.getUniqueId() + ".home.z");
+            float pitch = (float) config.getDouble("users." + player.getUniqueId() + ".home.pitch");
+            float yaw = (float) config.getDouble("users." + player.getUniqueId() + ".home.yaw");
+
+            Location location = new Location(player.getWorld(), x, y, z, yaw, pitch);
+
+            player.teleport(location);
+            player.sendMessage(ChatColor.GREEN + "Przeniesiono do domu");
+        } else {
+            player.sendMessage("§cNie posiadasz żadnego ustawionego domu!\nMożesz to zrobić używając komendy §7/sethome");
+        }
     }
 }
