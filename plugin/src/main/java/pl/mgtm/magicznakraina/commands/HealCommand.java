3a34279cf7de380baab0c97b8c72986ef4d45022
Fulerenn <maciej15s12@gmail.com>
2022-12-03 01:27:40 +0100

    Unique effect-ish messages?

--- plugin/src/main/java/pl/mgtm/magicznakraina/commands/HealCommand.java
+++ plugin/src/main/java/pl/mgtm/magicznakraina/commands/HealCommand.java
@@ -6,16 +6,27 @@ import org.bukkit.entity.Player;
 import pl.mgtm.magicznakraina.command.CommandInfo;
 import pl.mgtm.magicznakraina.command.PluginCommand;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 @CommandInfo(name = "heal", permission = "mgtm.heal", requiresPlayer = true)
 public class HealCommand extends PluginCommand {
-
     @Override
     public void execute(Player player, String[] args) {
         super.execute(player, args);
 
+        String[] messages = {
+                "Ale faza!",
+                "Łoooo magia :o",
+                "Czy ty to widzisz :O",
+                "Co tu się stało :OOO"
+        };
+
+        String message = ChatColor.DARK_GRAY + messages[new Random().nextInt(messages.length)];
+
         player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
         player.setFoodLevel(20);
 
-        player.sendMessage(ChatColor.GREEN + "Uleczono.");
+        player.sendMessage(message);
     }
 }
