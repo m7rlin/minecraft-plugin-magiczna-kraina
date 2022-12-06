e1151967a579aa66949b900848f2759f91743aef
Fulerenn <maciej15s12@gmail.com>
2022-12-06 02:12:23 +0100

    typo fixes

--- plugin/src/main/java/pl/mgtm/magicznakraina/commands/SerduszkoCommand.java
+++ plugin/src/main/java/pl/mgtm/magicznakraina/commands/SerduszkoCommand.java
@@ -8,26 +8,24 @@ import pl.mgtm.magicznakraina.MagicznaKraina;
 
 public class SerduszkoCommand implements CommandExecutor {
 
-    private MagicznaKraina pl = MagicznaKraina.getInstance();
+    private MagicznaKraina plugin = MagicznaKraina.getInstance();
 
     int[] levels = {1, 1, 1, 2, 3, 3, 4, 5, 5, 5, 5, 6, 7, 8, 9, 10, 10, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 20, 20, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 30, 30, 30, 31, 32, 33, 34, 35, 36, 37, 38, 38, 40};
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-
         if (cmd.getName().equalsIgnoreCase("serduszko")) {
             if (sender instanceof Player) {
                 Player p = (Player) sender;
 
-                double playerHearts = pl.getConfig().getInt("users." + p.getUniqueId() + ".hearts");
+                double playerHearts = plugin.getConfig().getInt("users." + p.getUniqueId() + ".hearts");
 
                 if (playerHearts >= 40.0) {
                     p.sendMessage("§bOsiągnąłeś(-aś) już maksymanlny poziom serduszek.");
                     return true;
                 }
 
-                int playerHeartsLevel = pl.getConfig().getInt("users." + p.getUniqueId() + ".heartsLevel");
-
+                int playerHeartsLevel = plugin.getConfig().getInt("users." + p.getUniqueId() + ".heartsLevel");
 
                 int playerLevel = p.getLevel();
 
@@ -57,7 +55,7 @@ public class SerduszkoCommand implements CommandExecutor {
     }
 
     private void addHearts(Player p, double playerHearts, int playerHeartsLevel) {
-        pl.getConfig().set("users." + p.getUniqueId() + ".hearts", playerHearts + 1.0);
+        plugin.getConfig().set("users." + p.getUniqueId() + ".hearts", playerHearts + 1.0);
 
         int maxLevels = levels.length - 1;
 
@@ -68,13 +66,13 @@ public class SerduszkoCommand implements CommandExecutor {
         }
 
         if (playerHeartsLevel < levels.length) {
-            pl.getConfig().set("users." + p.getUniqueId() + ".heartsLevel", playerHeartsLevel + 1);
+            plugin.getConfig().set("users." + p.getUniqueId() + ".heartsLevel", playerHeartsLevel + 1);
         }
 
         p.setMaxHealth(playerHearts + 1.0);
         p.setLevel(p.getLevel() - (levels[playerHeartsLevel] / 2));
         p.sendMessage("§aGratulacje! Dostajesz kolejne serduszko. Oby tak dalej.");
         p.sendMessage("Do nastepnego serduszka potrzebujesz §a" + nextPlayerLevelRequired + "§r poziomow XP.");
-        pl.saveConfig();
+        plugin.saveConfig();
     }
 }
