9c1d8c4d56e7b0b152a25dea07cb9fa9b48aa9d7
m7rlin <merlin@magictm.com>
2023-06-09 03:38:06 +0200

    remove unsused import

--- plugin/src/main/java/pl/mgtm/magicznakraina/modules/serduszko/events/PreLoginEvent.java
+++ plugin/src/main/java/pl/mgtm/magicznakraina/modules/serduszko/events/PreLoginEvent.java
@@ -1,16 +1,11 @@
 package pl.mgtm.magicznakraina.modules.serduszko.events;
 
-import org.bukkit.Bukkit;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import pl.mgtm.magicznakraina.MagicznaKraina;
 import pl.mgtm.magicznakraina.config.User;
-import pl.mgtm.magicznakraina.helpers.ConfigHelpers;
 
 public class PreLoginEvent implements Listener {
     private MagicznaKraina pl = MagicznaKraina.getInstance();
